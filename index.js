1a0dae112374d346c7dc6054930db50fca17b2eb
xEBUN <leaveyt@mail.ru>
2023-08-31 16:28:43 +0500

    last ver

--- index.js
+++ index.js
@@ -3,36 +3,86 @@ const app = express()
 const cors = require('cors')
 const fs = require("fs");
 var bodyParser = require('body-parser')
+const { Server } = require("socket.io");
 
 const corsOptions = {
     origin: '*',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token === '41cd5001-21e8-bce2-2dbb-8472fc0066f6') {
+       return next();
+    }
+    return res.sendStatus(401); // Unauthorized
+
+}
+
 app.use(cors(corsOptions))
 app.use(bodyParser.json())
-
-app.get('/api/get-islands', function (req, res) {
-    const data = fs.readFileSync('./islands.json',
-        { encoding: 'utf8' });
-    res.json({ islands: JSON.parse(data) }).status(200)
-})
+// app.use(authenticateToken);
 
 
-app.post('/api/set-session', function (req, res) {
-    const islands = fs.readFileSync('./islands.json',
-        { encoding: 'utf8' });
-    const parsedIslands = JSON.parse(islands)
-    const findedIsland = parsedIslands.find(island => island.name === req.body.island)
-    if(req.body.status === 1){
-        findedIsland.ATC.push(req.body.AtcPosition)
-    }
-    else{
-        findedIsland.ATC = findedIsland.ATC.filter(shift => shift != req.body.AtcPosition)
-    }
 
-    fs.writeFileSync('./islands.json', JSON.stringify(parsedIslands) )
-    res.json({ sessions: JSON.parse(islands) }).status(200)
-})
+const io = new Server({
+    cors: {
+        origin: "*"
+    },
+});
 
-app.listen(80)
+io.on('connection', (socket) => {
+    console.log('new user connected');
+    socket.on('getIslands', () => {
+        const data = fs.readFileSync('./islands.json',
+            {encoding: 'utf8'});
+
+        socket.emit('responseIslands',{islands: JSON.parse(data)})
+    });
+    socket.on('setSession', (payload) => {
+       console.log('CREATE SESSION LOG')
+        const parsedPayload = JSON.parse(payload)
+        const islands = fs.readFileSync('./islands.json',
+            {encoding: 'utf8'});
+        const parsedIslands = JSON.parse(islands)
+    //
+        const findedIsland = parsedIslands.find(island => island.name === parsedPayload?.island)
+        if (parsedPayload.status === 1) {
+            console.log('status 1')
+            findedIsland.ATC.push(parsedPayload?.AtcPosition)
+        } else if (parsedPayload.status === 0) {
+            findedIsland.ATC = findedIsland.ATC.filter(shift => shift !== parsedPayload?.AtcPosition)
+        }
+
+        fs.writeFileSync('./islands.json', JSON.stringify(parsedIslands))
+        socket.broadcast.emit('responseIslands', {islands: parsedIslands})
+    });
+    socket.on('setFlight', (payload) => {
+        const islands = fs.readFileSync('./islands.json',
+            {encoding: 'utf8'});
+        const parsedIslands = JSON.parse(islands)
+        const parsedPayload = JSON.parse(payload)
+
+        const findedIsland = parsedIslands.find(island => island.name === parsedPayload.from)
+        findedIsland.flights.push(parsedPayload)
+
+        fs.writeFileSync('./islands.json', JSON.stringify(parsedIslands))
+        socket.broadcast.emit('responseIslands',{islands: parsedIslands})
+    });
+    socket.on('endFlight', (payload) => {
+        const islands = fs.readFileSync('./islands.json',
+            {encoding: 'utf8'});
+        const parsedIslands = JSON.parse(islands)
+        const parsedPayload = JSON.parse(payload)
+
+        const findedIsland = parsedIslands.find(island => island.name === parsedPayload.from)
+        findedIsland.flights = findedIsland.flights.filter(flight => flight.user !== parsedPayload.user)
+
+        fs.writeFileSync('./islands.json', JSON.stringify(parsedIslands))
+        socket.broadcast.emit('responseIslands',{islands: parsedIslands})
+    })
+});
+
+io.listen(80);
